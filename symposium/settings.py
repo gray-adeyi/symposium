654d6068f6fd3452a3c9d657b7ce03a74003ad44
Gbenga Adeyi <adeyigbenga005@gmail.com>
2021-04-04 02:27:00 +0100

    Add config to serve media files in development

--- symposium/settings.py
+++ symposium/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# media files cofigs.
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Logging configs
 LOGGING = {
