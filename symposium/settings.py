73cb2329061024e93a1293341ae7662251e59009
Gbenga Adeyi <adeyigbenga005@gmail.com>
2021-04-08 10:38:37 +0100

    add log config for main app

--- symposium/settings.py
+++ symposium/settings.py
@@ -151,6 +151,11 @@ LOGGING = {
             'level': 'DEBUG',
             'propagate': True,
         },
+        'main': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
     },
 }
 
