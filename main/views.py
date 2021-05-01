fc0275e0a50a98395b5d5e93f76b2e4627b454c3
Gbenga Adeyi <adeyigbenga005@gmail.com>
2021-05-01 15:35:34 +0100

    Add handlers for error 404 and 500

--- main/views.py
+++ main/views.py
@@ -1,6 +1,9 @@
 from django.views import generic
+from django.shortcuts import render
 from . import models
-# Create your views here.
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class About(generic.TemplateView):
@@ -10,3 +13,12 @@ class About(generic.TemplateView):
         ctx = super().get_context_data(*args, **kwargs)
         ctx['site_info'] = models.SiteInfo.objects.get()
         return ctx
+
+
+def handler404(request, exception):
+    logger.error("Could not resolve path: ", str(exception.get('path')))
+    return render(request, 'main/404.html')
+
+
+def handler500(request):
+    return render(request, 'main/500.html')
--- symposium/urls.py
+++ symposium/urls.py
@@ -17,6 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+import main.views
+
+handler404 = main.views.handler404
+handler500 = main.views.handler500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
